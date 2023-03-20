c09e92e4d9663d26aa386c35318d457139f01bb1
Bekkhan <87779571856b@gmail.com>
2023-03-21 00:21:34 +0600

    midterm1

--- src/main.go
+++ src/main.go
@@ -1,100 +1,274 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-
-	//"html/template"
+	"html/template"
 	"net/http"
 )
 
-var host = ":8081"
+var host = ":8080"
 
-var db *gorm.DB
-var err error
-
-const DSN = "test.db"
-
-type User struct {
-	gorm.Model
-	Nickname   string `json:"nickname"`
-	Status     string `json:"status"`
-	FirstName  string `json:"firstname"`
-	SecondName string `json:"lastname"`
-	Password   string `json:"password"`
-	Age        uint16 `json:"age"`
+type Product struct {
+	Id          uint16
+	Brand       string
+	Type        string
+	Description string
+	Price       float32
+	Photo       string
 }
 
-func InitialMigration() {
-	db, err = gorm.Open(sqlite.Open(DSN), &gorm.Config{})
+var products = []Product{}
+var showProduct = Product{}
+
+func getProductData(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("SELECT * FROM `products`")
 	if err != nil {
-		fmt.Println(err.Error())
-		panic("cannot connect to db")
+		return nil, err
 	}
-	db.AutoMigrate(&User{})
+	defer rows.Close()
+
+	var item []Product
+	for rows.Next() {
+		var product Product
+		err = rows.Scan(&product.Id, &product.Brand, &product.Type, &product.Description, &product.Price, &product.Photo)
+		if err != nil {
+			return nil, err
+		}
+		item = append(item, product)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return item, nil
 }
 
-func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	data := mux.Vars(r)
-	var user User
-	db.First(&user, data["id"])
-	json.NewDecoder(r.Body).Decode(&user)
-	db.Save(&user)
-	json.NewEncoder(w).Encode(user)
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html", "templates/smallcard.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	//connecting db
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Get the product data from the database
+	products, err := getProductData(db)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", products)
+}
+func createUser(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/registration.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "registration", nil)
+}
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/login.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "login", nil)
+}
+func saveUser(w http.ResponseWriter, r *http.Request) {
+	nickname := r.FormValue("nickname")
+	firstname := r.FormValue("firstname")
+	lastname := r.FormValue("lastname")
+	age := r.FormValue("age")
+	password := r.FormValue("password")
+
+	if nickname == "" || firstname == "" || lastname == "" || age == "" || password == "" {
+		fmt.Fprintf(w, "You didnt write something")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `users` (`nickname`,`firstname`,`lastname`,`age`,`password`) VALUES('%s','%s','%s','%s','%s')", nickname, firstname, lastname, age, password))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+func checkUser(w http.ResponseWriter, r *http.Request) {
+	nickname := r.FormValue("nickname")
+	password := r.FormValue("password")
+
+	if nickname == "" || password == "" {
+		fmt.Fprintf(w, "You didnt write something")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		var exists bool
+		err = db.QueryRow(fmt.Sprintf("SELECT EXISTS(SELECT * from `users` WHERE `nickname`='%s' AND `password`='%s')", nickname, password)).Scan(&exists)
+		if err != nil {
+			panic(err)
+		}
+		if exists {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		} else {
+			fmt.Fprintf(w, "User don't exist")
+		}
+	}
+}
+func catalog(w http.ResponseWriter, r *http.Request) {
+	typeFilter := r.URL.Query().Get("type")
+	brandFilter := r.URL.Query().Get("brand")
+
+	t, err := template.ParseFiles("templates/header.html", "templates/footer.html", "templates/smallcard.html", "templates/filters.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	query := "SELECT * FROM `products`"
+	if typeFilter != "" {
+		query += " WHERE `type`='" + typeFilter + "'"
+	}
+	if brandFilter != "" {
+		if typeFilter == "" {
+			query += " WHERE "
+		} else {
+			query += " AND "
+		}
+		query += "`brand`='" + brandFilter + "'"
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	//iterate over results and write HTML for each row
+	var count = 0
+	buf := new(bytes.Buffer)
+	err = t.ExecuteTemplate(buf, "header", nil)
+	fmt.Fprintf(w, "%s", buf.String()) //header
+	if typeFilter != "" {
+		fmt.Fprintf(w, "<div class = \"badge\">\n    <a>Catalog</a>\n <a>Searched type of product: %s </a>\n </div>", typeFilter)
+	} else if brandFilter != "" {
+		fmt.Fprintf(w, "<div class = \"badge\">\n    <a>Catalog</a>\n <a>Searched brand of product: %s </a>\n </div>", brandFilter)
+	} else {
+		fmt.Fprintf(w, "<div class = \"badge\">\n    <a>Catalog</a>\n </div>")
+	}
+
+	fmt.Fprintf(w, "<div class =\"catalog-container\">")
+	buf = new(bytes.Buffer)
+	err = t.ExecuteTemplate(buf, "filters", nil)
+	fmt.Fprintf(w, "%s", buf.String()) //filters
+	fmt.Fprintf(w, "<div class=\"catalog-products\">")
+	fmt.Fprintf(w, "<div class =\"row\">")
+	for rows.Next() {
+		var id uint16
+		var brand string
+		var productType string
+		var description string
+		var price float32
+		var photoAddress string
+
+		err = rows.Scan(&id, &brand, &productType, &description, &price, &photoAddress)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if count == 3 {
+			fmt.Fprintf(w, "</div>")
+			fmt.Fprintf(w, "<div class=\"row\">")
+			count = 0
+		}
+		// write HTML for product row
+		fmt.Fprintf(w, "<div class=\"card\">\n  <div class=\"card-header\">\n    <h2>%s</h2>\n  </div>\n  <div class=\"card-image\">\n    <img src=\"/static/images/products/%s\" alt=\"some product\">\n  </div>\n  <p>Price: $%0.2f</p>\n  <div class=\"card-description\">\n    <p>%s</p>\n  </div>\n  <form>\n    <button formaction=\"/product/%d\">Product Overview</button>\n <button>Add to cart</button>\n  </form>\n</div>", brand, photoAddress, price, description, id)
+		count += 1
+	}
+	fmt.Fprintf(w, "</div>")
+	fmt.Fprintf(w, "</div>")
+	buf = new(bytes.Buffer)
+	err = t.ExecuteTemplate(buf, "footer", nil)
+	fmt.Fprintf(w, "%s", buf.String()) //footer
 }
 
-func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	data := mux.Vars(r)
-	var user User
-	db.Delete(&user, data["id"])
-	json.NewEncoder(w).Encode("User successfully deleted")
+func productFullInfo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	t, err := template.ParseFiles("templates/product.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//vyborka dannyh
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM `products` WHERE `id` = '%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var item Product
+	for rows.Next() {
+		var product Product
+		err = rows.Scan(&product.Id, &product.Brand, &product.Type, &product.Description, &product.Price, &product.Photo)
+		if err != nil {
+			panic(err)
+		}
+		item = product
+	}
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(w, "product", item)
 }
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var users []User
-	db.Find(&users)
-	json.NewEncoder(w).Encode(users)
-}
-
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	data := mux.Vars(r)
-	var user User
-	db.First(&user, data["id"])
-	json.NewEncoder(w).Encode(user)
-}
-
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var user User
-	json.NewDecoder(r.Body).Decode(&user)
-	db.Create(&user)
-	json.NewEncoder(w).Encode(user)
-}
-
-/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-
-func handleRequests() {
+func handleFunc() {
 	r := mux.NewRouter()
-	//r.HandleFunc("/", mainPage) - how to use templates in net/http
-	//r.HandleFunc("/registration/", registrationPage) - how to use templates in net/http
-	r.HandleFunc("/users", GetUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/registration", createUser).Methods("GET")
+	r.HandleFunc("/save_user", saveUser).Methods("POST")
+	r.HandleFunc("/login", loginUser).Methods("GET")
+	r.HandleFunc("/check_user", checkUser).Methods("POST")
+	r.HandleFunc("/catalog", catalog).Methods("GET")
+	r.HandleFunc("/product/{id:[0-9]+}", productFullInfo).Methods("GET")
 
-	fmt.Printf("Server is listening on host %s ... \n", host)
-	http.ListenAndServe(host, r)
+	http.Handle("/", r)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	fmt.Printf("server is listening on host %s \n", host)
+	http.ListenAndServe(host, nil)
 }
 
 func main() {
-	InitialMigration()
-	handleRequests()
+	handleFunc()
 }
